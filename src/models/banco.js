b37044a5fd198f1c320656ee5f72dfd6512ea2e9
Edu1059 <eduardoaffkaspro@gmail.com>
2024-12-19 21:40:02 -0300

    add env_example and fix bugs

--- src/models/banco.js
+++ src/models/banco.js
@@ -1,38 +1,46 @@
 const fs = require('fs')
 
-let id = 1;
 let users = [];
 
 load()
 class BancoUsers {
-
-    getUsers() {
-        return users;
+    
+    incrementa_id() {
+        for(let i = 0; i <= users.length; i++) {
+            if(i == users.length) {
+                return ++i;
+            }
+        }
     }
 
+    getUsers() {
+        console.log(users.length);
+        return users;
+    }
+    
     // Criar usuários não administradores
     createUser (usuario, senha) {
         let newUsers = {
-            id: id++,
+            id: this.incrementa_id(),
             usuario: usuario,
             senha: senha,
             isAdmin: false
         }
-
+        
         users.push(newUsers);
         save()
         return users;
     }
-
+    
     // Criar usuários administradores
     createUserAdmin (usuario, senha) {
         let newUsers = {
-            id: id++,
+            id: this.incrementa_id(),
             usuario: usuario,
             senha: senha,
             isAdmin: true
         }
-
+        
         users.push(newUsers);
         save();
         return users;
@@ -54,7 +62,7 @@ class BancoUsers {
         for(let i = 1; i < users.length; i++) {
             if(users[i].id == id && users[i].isAdmin == false) {
                 users.splice(i, 1);
-                save()
+                save();
                 return `Usuário ID ${id} deletado com sucesso`;
             } 
         }
@@ -92,8 +100,7 @@ class BancoUsers {
 }
 
 function save() {
-    console.log(users)
-    fs.writeFile('./src/models/data/file.json', JSON.stringify(users, null, 2), (err) => {
+    fs.writeFile('./src/models/data/user.json', JSON.stringify(users, null, 2), (err) => {
         if (err) {
             console.error('Erro ao salvar os dados:', err);
         } else {
@@ -103,7 +110,7 @@ function save() {
 }
 
 function load(){
-    const data = fs.readFileSync('./src/models/data/file.json','utf-8', (err) =>{
+    const data = fs.readFileSync('./src/models/data/user.json','utf-8', (err) =>{
         if(err){
             console.error("Erro ao carregar os dados:", err);
             return;
